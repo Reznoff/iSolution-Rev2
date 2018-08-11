2260768188ebe2b417b6c3f90dc43cb5ef55c387
Kemal Maulana <kemalmaulana19@gmail.com>
2018-08-11 09:34:45 +0700

    Fixed Splash, Login, Main Activity bugged as fuck

---
+++ app/src/main/java/com/example/kemalmaulana/isolution/Helper/PojoJadwalPelajaran.kt
@@ -0,0 +1,3 @@
+package com.example.kemalmaulana.isolution.Helper
+
+class PojoJadwalPelajaran(val hari: String, val nama_pelajaran: String, val pengajar: String, val kurikulum: String, val kelas: String, val jam_masuk: String, val jam_keluar: String, val tahun_pelajaran: String)
--- app/src/main/java/com/example/kemalmaulana/isolution/Helper/UserSession.kt
+++ app/src/main/java/com/example/kemalmaulana/isolution/Helper/UserSession.kt
@@ -1,71 +1,26 @@
 package com.example.kemalmaulana.isolution.Helper
 
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
-import android.util.Log
-import com.example.kemalmaulana.isolution.Activity.LoginActivity
-import com.example.kemalmaulana.isolution.Activity.MainActivity
 
-open class UserSession(context: Context) {
+open class UserSession {
+    companion object {
+        lateinit var preference: SharedPreferences
+        var PREF_NAME: String = "Session"
+        open fun createSignInSession(context: Context, nis: String) {
+            preference = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE) ?: return
+            with(preference.edit()) {
+                putString(context.getString(com.example.kemalmaulana.isolution.R.string.nis), nis)
+                commit()
+            }
+        }
 
-    var prefs: SharedPreferences
-    var editor: SharedPreferences.Editor
-    var thisContext: Context
-
-    val PRIVATE_MODE: Int = 0
-    val PREFER_NAME: String = "Session"
-    val IS_USER_LOGGED_IN: String = "IsLogin"
-    val KEY_NAME: String = "admin"
-    val KEY_PASS: String = "admin"
-
-    init {
-        thisContext = context
-        prefs = thisContext.getSharedPreferences(PREFER_NAME, PRIVATE_MODE)
-        editor = prefs.edit()
-    }
-
-    open fun createUserLoginSession(keyName: String, keyPass: String) {
-        if(keyName.equals(KEY_NAME) && keyPass.equals(KEY_PASS)) {
-            editor.putBoolean(IS_USER_LOGGED_IN, true)
-            editor.putString(KEY_NAME, keyName)
-            editor.putString(KEY_PASS, keyPass)
-            editor.commit()
+        open fun logoutSession(context: Context) {
+            preference = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE) ?: return
+            with(preference.edit()) {
+                clear()
+                commit()
+            }
         }
     }
-
-    open fun isUserLoggedIn(): Boolean {
-        Log.d("IsLogged", prefs.getBoolean(IS_USER_LOGGED_IN, true).toString())
-        return prefs.getBoolean(IS_USER_LOGGED_IN, true)
-    }
-
-    open fun checkLogin(): Boolean {
-        if(!this.isUserLoggedIn()) {
-            val i: Intent = Intent(thisContext, MainActivity::class.java)
-            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            thisContext.startActivity(i)
-            return true
-        } else {
-            return false
-        }
-    }
-
-    open fun getUserDetails(): HashMap<String, String> {
-        val user: HashMap<String, String> = HashMap()
-        user.put(KEY_NAME, prefs.getString(KEY_NAME, null))
-        return user
-    }
-
-    open fun logoutUser() {
-        editor.clear()
-        editor.commit()
-
-        val i: Intent = Intent(thisContext, LoginActivity::class.java)
-        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        thisContext.startActivity(i)
-    }
-
-
 }
